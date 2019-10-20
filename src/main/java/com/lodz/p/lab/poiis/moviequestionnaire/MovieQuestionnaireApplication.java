66611d53b5f963819eca8f9a1d5780b72c788307
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-10-20 23:05:55 +0200

    added files

---
+++ src/main/java/com/lodz/p/lab/poiis/moviequestionnaire/MovieQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.lodz.p.lab.poiis.moviequestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieQuestionnaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieQuestionnaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lodz/p/lab/poiis/moviequestionnaire/MovieQuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lodz.p.lab.poiis.moviequestionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieQuestionnaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
