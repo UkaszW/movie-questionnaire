92c7642195dc4de8c70bb5dd77cd111c666e1bd4
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-10-20 23:11:23 +0200

    added simple main vaadin view

---
+++ src/main/java/com/lodz/p/lab/poiis/moviequestionnaire/frontend/MainView.java
@@ -0,0 +1,14 @@
+package com.lodz.p.lab.poiis.moviequestionnaire.frontend;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add(new Button("Click me", buttonClickEvent -> Notification.show("Clicked!")));
+    }
+}
